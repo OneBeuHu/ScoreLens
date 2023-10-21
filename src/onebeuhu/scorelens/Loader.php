02afb6f9b3e8d5a5a56774bbd4d5fbafc7d0525c
Ilya Taranov <109813776+OneBeuHu@users.noreply.github.com>
2023-10-21 23:39:53 +0300

    Removed the unused use

--- src/onebeuhu/scorelens/Loader.php
+++ src/onebeuhu/scorelens/Loader.php
@@ -4,7 +4,6 @@ namespace onebeuhu\scorelens;
 
 use onebeuhu\scorelens\command\BoardCommand;
 use onebeuhu\scorelens\listener\EventListener;
-use onebeuhu\scorelens\task\UpdateBoardTask;
 use pocketmine\plugin\PluginBase;
 
 class Loader extends PluginBase
